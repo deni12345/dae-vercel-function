ee6f9055143101f245588eb389a359a130384a1a
liemtx <bebu1407@gmail.com>
2025-06-03 23:50:20 +0700

    LT#Refactor code structure and improve API functionality
    - Updated .gitignore to include Go files and other necessary files.
    - Refactored API handlers for better readability and consistency.
    - Enhanced error handling in Firestore operations.
    - Improved the HTML structure and styling for the API test page.
    - Updated JavaScript to handle API calls more efficiently and provide better user feedback.
    - Modified Vercel configuration for improved routing and function handling.
    - Added missing functionality in Firestore methods for better data management.

--- static/script.js
+++ static/script.js
@@ -3,16 +3,19 @@ const API_ENDPOINTS = [
     id: "health",
     name: "Health Check",
     path: "/api/health",
+    method: "GET",
   },
   {
     id: "sheet-listen",
     name: "Listen Sheet",
     path: "/api/sheet-listen",
+    method: "GET",
   },
   {
     id: "collections",
     name: "Collections",
     path: "/api/collections",
+    method: "GET",
   },
 ];
 
@@ -28,19 +31,26 @@ function generateApiComponents() {
 
   API_ENDPOINTS.forEach((endpoint) => {
     const componentHtml = `
-        <button class="button" onclick="callApi('${endpoint.id}')">
-          Get ${endpoint.name}
-        </button>
+    <div class="api-component">
+       <div class="api-component-header">
+          <button class="button" onclick="callApi('${endpoint.id}')">
+             ${endpoint.method}
+          </button>
+        <div class="api-status">
+          <h4 id="text-status-${endpoint.id}">Status: </h4>
+          <span class="dot-status" id="dot-status-${endpoint.id}"></span>
+        </div>
+      </div>
         <div class="result-container">
           <div class="result-header" onclick="toggleResult('result-${endpoint.id}')">
-            <span>${endpoint.name} Result</span>
+            <span>Response</span>
             <span class="toggle-icon"></span>
           </div>
           <div id="result-${endpoint.id}" 
-               class="result" 
-               placeholder="Result will appear here...">
+               class="result">
           </div>
-        </div>`;
+          </div>
+      </div>`;
     container.insertAdjacentHTML("beforeend", componentHtml);
   });
 }
@@ -48,21 +58,26 @@ function generateApiComponents() {
 async function callApi(id) {
   const endpoint = API_ENDPOINTS.find((e) => e.id === id);
   const resultDiv = document.getElementById(`result-${id}`);
-  resultDiv.innerHTML = "";
 
   try {
-    const response = await axios.get(endpoint.path);
-    const dotId = `status-${id}`;
-    resultDiv.innerHTML = `
-        <h4>Status: ${response.status}<span id=${dotId} class="dot"></span></h4>
-        <h3>Response:</h3>
-        <pre>${JSON.stringify(response.data, null, 2)}</pre>`;
-    document.getElementById(dotId).style.background = "green";
-
     resultDiv.classList.add("expanded");
+    resultDiv.innerHTML = `<p>Loading...</p>`;
+    const response = await axios.get(endpoint.path);
+    resultDiv.innerHTML = `<pre>${JSON.stringify(
+      response.data,
+      null,
+      2
+    )}</pre>`;
+    document.getElementById(
+      `text-status-${id}`
+    ).textContent = `Status: ${response.status}`;
+    document.getElementById(`dot-status-${id}`).style.backgroundColor = "green";
+
     resultDiv.previousElementSibling.classList.add("expanded");
   } catch (error) {
     resultDiv.innerHTML = `<h3>Error:</h3><p>${error.message}</p>`;
+    document.getElementById(`text-status-${id}`).textContent = `Status: Error`;
+    document.getElementById(`dot-status-${id}`).style.backgroundColor = "red";
   }
 }
 
